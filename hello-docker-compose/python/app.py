89e7a384bd5e9491349747c69986f20d0a8ee857
Tanay Bhartia <tanaybhartia@microsoft.com>
2021-09-28 18:03:45 +0530

    new docker compose

--- hello-docker-compose/python/app.py
+++ hello-docker-compose/python/app.py
@@ -11,7 +11,7 @@ dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
 
 n = 0
-while (n < 0):
+while True:
     n += 1
     message = {"data": {"orderId": n}}
 
